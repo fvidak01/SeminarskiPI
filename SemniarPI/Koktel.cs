5fa6f1b537e08dfde50177791b843c082824ef9d
Ivan <kula124@gmail.com>
2017-12-10 14:28:47 +0100

    Class added

---
+++ SemniarPI/Koktel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemniarPI
+{
+    class Koktel
+    {
+        private string _ime;
+        private string _opis;
+        private string _upute;
+        private byte[] _slika;
+
+        public Koktel(object[] arr)
+        {
+            var i = 0;
+            _ime = (string)arr[i++];
+            _opis = (string)arr[i++];
+            _upute = (string)arr[i++];
+            _slika = (byte[])arr[i];
+        }
+
+        public string Ime => _ime;
+
+        public string Opis => _opis;
+
+        public string Upute => _upute;
+
+        public Image Slika
+        {
+            get
+            {
+                var ms = new MemoryStream(_slika);
+                return Image.FromStream(ms);
+            }
+        }
+
+        public static List<Koktel> CreateKoktailList(List<object[]> rawInput)
+        {
+            if (rawInput is null)
+                throw new ArgumentNullException(nameof(rawInput));
+            var list = new List<Koktel>();
+            foreach (object[] t in rawInput)
+            {
+                var item = new Koktel(t);
+                list.Add(item);
+            }
+            return list;
+        }
+    }
+}
---
+++ SemniarPI/Sastojci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemniarPI
+{
+    class Sastojci
+    {
+        private string _ime;
+        private string _napomena;
+        private byte[] _slika;
+
+        public string Ime { get => _ime; set => _ime = value; }
+        public string Napomena { get => _napomena; set => _napomena = value; }
+        public byte[] Slika { get => _slika; set => _slika = value; }
+    }
+
+}
