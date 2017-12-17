3ed84f230c13783a06b629ed36f9b72a7c1e837b
Ivan <kula124@gmail.com>
2017-12-17 23:21:18 +0100

    Added missing Sastojci function and few reworks Querys moved to const filed, string.replace used to construct SQL querys

--- SemniarPI/DBaccess.cs
+++ SemniarPI/DBaccess.cs
@@ -12,7 +12,12 @@ namespace SemniarPI
     static class DBaccess
     {
         private const string QUERYselectAllMaxPairs = "SELECT K_FK as KoktelD, COUNT(*) as NumberOfIngridiens FROM Veze GROUP BY(K_FK)";
+
+        private const string QUERYselectMyKoktelSastojciPairs =
+            "SELECT K_PK,COUNT(*) as Times FROM (SELECT K_PK,Ime,Opis,Upute,Slika FROM Kokteli INNER JOIN Veze V ON Kokteli.K_PK = V.K_FK Where (S_FK in (<LIST>))) GROUP BY K_PK";
+        private const string QUERYselectMissingSastojciInMyKoktels = "SELECT S_PK,Ime,Napomena,Slika FROM Sastojci INNER JOIN(SELECT K_FK, S_FK from Veze where K_FK = <KID> EXCEPT SELECT K_FK, S_FK from Veze where S_FK in (<LIST>)) as Mid on S_PK = Mid.S_FK";
         private static bool _isOpen;
+        public static int Tolerance = 5; //TODO: Encapsulate this fucker (to gui)
         public enum Table
         {
             Kokteli,
@@ -86,23 +91,20 @@ namespace SemniarPI
                 list.Add(row);
             }
             return list;
+            //return list.Count == 0 ? null : list;
         }
 
         public static List<Koktel> GetMyKoktels(List<Sastojci> mySastojcis)
         {
             //Get number of ing for each coctail
             List<int[]> KoktelSastocjiNumberPairs = GetTwoIntPairs(QUERYselectAllMaxPairs);
-            //Get number of ing contained in each Koktel based on inputed mySastojci
-            //Get PK from mySastojci objects
+            //Get number of ing contained in each Koktel based on inputed mySastojci in 3 steps
+            //1) Get PK from mySastojci objects
             var listQuery = "";
             mySastojcis.ForEach(x => listQuery += x.Id.ToString() + ",");
-            var array = listQuery.ToCharArray();
-            array[listQuery.LastIndexOf(',')] = ' ';
-            listQuery = new string(array);
-            List<int[]> MyKoktelSastocjiNumberPairs =
-                GetTwoIntPairs(
-                    "SELECT K_PK,COUNT(*) as Times FROM (SELECT K_PK,Ime,Opis,Upute,Slika FROM Kokteli INNER JOIN Veze V ON Kokteli.K_PK = V.K_FK Where (S_FK in (" + listQuery + "))) GROUP BY K_PK");
-            //Compare the two and get full Koktels; TODO: Add tolerance n shit
+            listQuery = listQuery.Remove(listQuery.Length - 1);
+            List<int[]> MyKoktelSastocjiNumberPairs = GetTwoIntPairs(QUERYselectMyKoktelSastojciPairs.Replace("<LIST>", listQuery));
+            //2) Compare the two and get valid Koktels relative to Tolerance; TODO: Add <real> tolerance n shit
             var theChosenOnes = new List<int>();
             foreach (var o in KoktelSastocjiNumberPairs)
             {
@@ -110,16 +112,14 @@ namespace SemniarPI
                 if ((thePair = MyKoktelSastocjiNumberPairs.FirstOrDefault(x => x[0] == o[0])) == null)
                     continue;
                 //Add to final list
-                if (o[1] - thePair[1] <= 0) //0->Tolerance, in this case 0 TODO: Create tolerance field
+                if (o[1] - thePair[1] <= Tolerance) //0->Tolerance, in this case 0 TODO: Create <real> tolerance field
                     theChosenOnes.Add(thePair[0]); //Add ID to the list of requested Koktels
             }
-            //Get IDs to a string
+            //2.1 Get IDs of Koktels passing the test above (SUB <= Tolerance)
             var finalListQuery = "";
             theChosenOnes.ForEach(x => finalListQuery += x.ToString() + ',');
-            var finalArray = finalListQuery.ToCharArray();
-            finalArray[finalListQuery.LastIndexOf(',')] = ' ';
-            finalListQuery = new string(finalArray);
-            //Run the final query 
+            finalListQuery = finalListQuery.Remove(finalListQuery.Length - 1);
+            //3) Use IDs to get Koktels from the DB 
             var theFinalList = SelectAll("SELECT * FROM Kokteli WHERE K_PK in (" + finalListQuery + ")");
             return Koktel.CreateKoktailList(theFinalList);
         }
@@ -133,6 +133,27 @@ namespace SemniarPI
                 myList.Add(new[] { int.Parse(pair[0].ToString()), int.Parse(pair[1].ToString()) }); //Possible OverflowExcp due to unchecked long=>int convertion.
             } //^ should work fine given that the DB is quite small (certanly fucking smaller than MAX_INT). Will test
             return myList;
+        }
+
+        public static Dictionary<long, List<Sastojci>> GetMissingSastojciForKoktelList(List<Koktel> myKoktels, List<Sastojci> mySastojcis)
+        {
+            string listQuery = "";
+            mySastojcis.ForEach(x => listQuery += x.Id.ToString() + ",");
+            listQuery = listQuery.Remove(listQuery.Length - 1);
+            string rawQuery = QUERYselectMissingSastojciInMyKoktels;
+            var Pairs = new Dictionary<long, List<Sastojci>>();
+            foreach (var koktel in myKoktels)
+            {
+                var newQuery = rawQuery.Replace("<KID>", koktel.ID.ToString()).Replace("<LIST>", listQuery);
+                var row = SelectAll(newQuery);
+                if (row is null)
+                {
+                    var shit = new Exception("I took a dumb");
+                    throw shit; //ha ha look, me funny X)
+                }
+                Pairs.Add(koktel.ID, Sastojci.CreateSastojciList(row));
+            }
+            return Pairs;
         }
     }
 }
