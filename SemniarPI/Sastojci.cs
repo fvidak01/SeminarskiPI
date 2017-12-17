532c7bf8e82c31376edc6f9076886ec828f42cb0
Ivan <kula124@gmail.com>
2017-12-17 20:29:22 +0100

    Class finished analog to Koktel class

--- SemniarPI/Sastojci.cs
+++ SemniarPI/Sastojci.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SemniarPI
 {
-    class Sastojci
+    public class Sastojci
     {
+        private readonly long _id;
         private string _ime;
         private string _napomena;
         private byte[] _slika;
-
+        public Sastojci(object[] arr)
+        {
+            var i = 0;
+            _id = (long)arr[i++];
+            _ime = (string)arr[i++];
+            _napomena = (string)arr[i++];
+            _slika = (byte[])arr[i];
+        }
         public string Ime { get => _ime; set => _ime = value; }
         public string Napomena { get => _napomena; set => _napomena = value; }
-        public byte[] Slika { get => _slika; set => _slika = value; }
+        public Image Slika
+        {
+            get
+            {
+                if (_slika is null)
+                    return null;
+                var ms = new MemoryStream(_slika);
+                return Image.FromStream(ms);
+            }
+        }
+        public long Id => _id;
+        public static List<Sastojci> CreateSastojciList(List<object[]> rawInput)
+        {
+            if (rawInput is null)
+                throw new ArgumentNullException(nameof(rawInput));
+            var list = new List<Sastojci>();
+            foreach (object[] t in rawInput)
+            {
+                var item = new Sastojci(t);
+                list.Add(item);
+            }
+            return list;
+        }
     }
 
 }
