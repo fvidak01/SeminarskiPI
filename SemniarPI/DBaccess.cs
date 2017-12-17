8bb4c67cc32adbbee6bfd04550a0fd92060beac7
Ivan <kula124@gmail.com>
2017-12-17 20:27:04 +0100

    Added main function to get the Koktels based on Sastojci list. Added methods to assist that

--- SemniarPI/DBaccess.cs
+++ SemniarPI/DBaccess.cs
@@ -11,7 +11,8 @@ namespace SemniarPI
 {
     static class DBaccess
     {
-        private static bool _isOpen = false;
+        private const string QUERYselectAllMaxPairs = "SELECT K_FK as KoktelD, COUNT(*) as NumberOfIngridiens FROM Veze GROUP BY(K_FK)";
+        private static bool _isOpen;
         public enum Table
         {
             Kokteli,
@@ -37,8 +38,7 @@ namespace SemniarPI
             }
             return _isOpen;
         }
-
-        public static List<object> SelectAll(Table tablica)
+        public static List<object[]> SelectAll(Table tablica)
         {
             if (!_isOpen)
                 return null; //TODO: Open connection or throw
@@ -58,7 +58,7 @@ namespace SemniarPI
                     throw new ArgumentOutOfRangeException(nameof(tablica), tablica, null); //TODO: not
             }
             var reader = sqliteCmd.ExecuteReader();
-            var list = new List<object>();
+            var list = new List<object[]>();
             while (reader.Read())
             {
                 object[] row = new object[reader.FieldCount];
@@ -69,6 +69,70 @@ namespace SemniarPI
                 list.Add(row);
             }
             return list;
+        }
+        public static List<object[]> SelectAll(string query)
+        {
+            var sqliteCmd = _sqliteConn.CreateCommand();
+            sqliteCmd.CommandText = query;
+            var reader = sqliteCmd.ExecuteReader();
+            var list = new List<object[]>();
+            while (reader.Read())
+            {
+                object[] row = new object[reader.FieldCount];
+                for (var i = 0; i < reader.FieldCount; i++)
+                {
+                    row[i] = reader.GetValue(i);
+                }
+                list.Add(row);
+            }
+            return list;
+        }
+
+        public static List<Koktel> GetMyKoktels(List<Sastojci> mySastojcis)
+        {
+            //Get number of ing for each coctail
+            List<int[]> KoktelSastocjiNumberPairs = GetTwoIntPairs(QUERYselectAllMaxPairs);
+            //Get number of ing contained in each Koktel based on inputed mySastojci
+            //Get PK from mySastojci objects
+            var listQuery = "";
+            mySastojcis.ForEach(x => listQuery += x.Id.ToString() + ",");
+            var array = listQuery.ToCharArray();
+            array[listQuery.LastIndexOf(',')] = ' ';
+            listQuery = new string(array);
+            List<int[]> MyKoktelSastocjiNumberPairs =
+                GetTwoIntPairs(
+                    "SELECT K_PK,COUNT(*) as Times FROM (SELECT K_PK,Ime,Opis,Upute,Slika FROM Kokteli INNER JOIN Veze V ON Kokteli.K_PK = V.K_FK Where (S_FK in (" + listQuery + "))) GROUP BY K_PK");
+            //Compare the two and get full Koktels; TODO: Add tolerance n shit
+            var theChosenOnes = new List<int>();
+            foreach (var o in KoktelSastocjiNumberPairs)
+            {
+                var thePair = new int[2];
+                if ((thePair = MyKoktelSastocjiNumberPairs.FirstOrDefault(x => x[0] == o[0])) == null)
+                    continue;
+                //Add to final list
+                if (o[1] - thePair[1] <= 0) //0->Tolerance, in this case 0 TODO: Create tolerance field
+                    theChosenOnes.Add(thePair[0]); //Add ID to the list of requested Koktels
+            }
+            //Get IDs to a string
+            var finalListQuery = "";
+            theChosenOnes.ForEach(x => finalListQuery += x.ToString() + ',');
+            var finalArray = finalListQuery.ToCharArray();
+            finalArray[finalListQuery.LastIndexOf(',')] = ' ';
+            finalListQuery = new string(finalArray);
+            //Run the final query 
+            var theFinalList = SelectAll("SELECT * FROM Kokteli WHERE K_PK in (" + finalListQuery + ")");
+            return Koktel.CreateKoktailList(theFinalList);
+        }
+
+        private static List<int[]> GetTwoIntPairs(string query)
+        {
+            var rawList = SelectAll(query);
+            var myList = new List<int[]>();
+            foreach (var pair in rawList)
+            {
+                myList.Add(new[] { int.Parse(pair[0].ToString()), int.Parse(pair[1].ToString()) }); //Possible OverflowExcp due to unchecked long=>int convertion.
+            } //^ should work fine given that the DB is quite small (certanly fucking smaller than MAX_INT). Will test
+            return myList;
         }
     }
 }
