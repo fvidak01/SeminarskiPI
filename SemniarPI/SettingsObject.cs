8bbcbc18a513e05adc860e65973130f729e6328f
Ivan <kula124@gmail.com>
2017-12-17 20:25:05 +0100

    Added Logger class Added Settings Object class

---
+++ SemniarPI/SettingsObject.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SemniarPI
+{
+    class SettingsObject
+    {
+        public event EventHandler ApperenceChanged;
+        public event EventHandler LoggingSwitched;
+        public event EventHandler DatabaseChanged; //TODO: Implement and use
+        public enum FormTheme
+        {
+            Black,
+            White
+        }
+        public enum FormStyle
+        {
+            //TODO: Add all styles
+            Blue,
+            Teral
+        }
+
+        private bool _logging;
+        private FormTheme _theme;
+        private FormStyle _style;
+        private FileInfo _selectedDb;
+
+        public FileInfo SelectedDb
+        {
+            get => _selectedDb;
+            set => _selectedDb = value; //TODO: Fire event
+        }
+
+        public bool EnableLogging
+        {
+            get => _logging;
+            set
+            {
+                if (value == _logging)
+                    return;
+                _logging = value;
+                this.LoggingSwitched?.Invoke(this, null);
+
+            }
+        }
+
+        public FormTheme Theme
+        {
+            get => _theme;
+            set
+            {
+                _theme = value;
+                this.ApperenceChanged?.Invoke(this, null);
+            }
+        }
+
+        public FormStyle Style
+        {
+            get => _style;
+            set
+            {
+                _style = value;
+                this.ApperenceChanged?.Invoke(this, null);
+            }
+        }
+    }
+}
